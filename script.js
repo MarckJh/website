316264cd9cc92f66cc0557a560f6da841dadd56a
Marck <marckjhbanquero@gmail.com>
2025-03-31 16:38:13 +0800

    secret update

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const audio = document.getElementById('myAudio');
-    const playPauseBtn = document.getElementById('playPause')
-    if (!audio) {
-      console.error('Audio element not found!');
-      return;
-    }
-    if (!playPauseBtn) {
-      console.error('Button not found!');
-      return;
-    }
+  
+const audio = document.getElementById('myAudio');
+const playPauseBtn = document.getElementById('playPause')
+const toggleButton = document.getElementById('toggleButton')
+const imageContainer = document.getElementById('imageContainer')
 
-    playPauseBtn.addEventListener('click', function() {
-      if (audio.paused) {
-        audio.play()
-          .then(() => {
-            console.log("Song playing.")
-          })
-          .catch(error => {
-            console.error('Playback failed:', error);
-          });
-      } else {
-        audio.pause();
-        console.log("Song paused.")
+toggleButton.addEventListener('click', function(){
+  console.log("Toggled pictures!")
+  imageContainer.classList.toggle('hidden')
+})
+
+if (!audio) {
+    console.error('Audio element not found!');
+return;
+}
+    
+if (!playPauseBtn) {
+    console.error('Button not found!');
+    return;
+}
+
+playPauseBtn.addEventListener('click', function() {
+if (audio.paused) {
+  audio.play()
+.then(() => {
+  console.log("Song playing.")
+  playPauseBtn.classList.toggle("colorChange")
+})
+.catch(error => {
+  console.error('Playback failed:', error);
+});
+}
+
+else {
+  audio.pause();
+  console.log("Song paused.")
+  playPauseBtn.classList.toggle("colorChange")
       }
     });
   });
-
-const imageToggle = DocumentType.getElementById("imageToggle")
-
-function 
